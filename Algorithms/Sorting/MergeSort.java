4a987355f7ab77162566e42f377515ca223006f3
srn22 <ramnathpurijala@gmail.com>
2023-02-25 20:57:09 +0530

    Added comments to MergeSort and QuickSort

--- Algorithms/Sorting/MergeSort.java
+++ Algorithms/Sorting/MergeSort.java
@@ -1,33 +1,43 @@
 import java.util.Arrays;
 class MergeSort {
-    void conquer(int[] arr, int[] aux, int l, int mid, int r) {
-        for(int i=l; i<=r; i++) aux[i] = arr[i];
-        int left = l;
+    // MergeSort - Divide and Conquer Algorithm
+
+    // MergeSort works by dividing the array into halves recursively until the array can no longer be divided -
+    // - and then merges the all divided arrays in sorted order. This divide and conquer method results in a -
+    // - very efficient running time and is also very stable.
+
+    // Time Complexity - O(n log n) (O(log n) for the divide and O(n) for the merge)
+    // Space Complexity - O(n) (for an auxiliary array for the merging and also the recursion call stack)
+
+    void conquer(int[] arr, int[] aux, int l, int mid, int r) {                         // Merge function for the two sorted arrays
+        for(int i=l; i<=r; i++) aux[i] = arr[i];                                        // Copying over all values into the auxiliary array
+        int left = l;                                                                   // Left bounds and right bounds of the sorted array1
         int right = mid;
-        l = mid+1;
-        int i = left;
-        while(l <= r && left <= right) {
-            if(aux[l] < aux[left]) arr[i++] = aux[l++];
-            else arr[i++] = aux[left++];
-        }
-        while(l <= r) arr[i++] = aux[l++];
-        while(left <= right) arr[i++] = aux[left++];
+        l = mid+1;                                                                      // Left bounds and right bounds of the sorted array2
+        int i = left;                                                                   // Current index of the original array, which will have -
+                                                                                        // - the sorted output at the end.
+        while(l <= r && left <= right) {                                                // Merging two sorted arrays logic
+            if(aux[l] < aux[left]) arr[i++] = aux[l++];                                 // We copy over the lower value at left pointer of array1
+            else arr[i++] = aux[left++];                                                // - or array2 to current index of the original array -
+        }                                                                               // - and increment the current and left pointer
+        while(l <= r) arr[i++] = aux[l++];                                              // We then check if any of the values are leftover, and -
+        while(left <= right) arr[i++] = aux[left++];                                    // - copy them over, because they are already in sorted order.
     }
-    void divide(int[] arr, int[] aux, int l, int r) {
-        if(l >= r) return;
-        int mid = l + (r-l) / 2;
-        divide(arr, aux, l, mid);
-        divide(arr, aux, mid+1, r);
-        conquer(arr, aux, l, mid, r);
+    void divide(int[] arr, int[] aux, int l, int r) {                                   // Dividing the array into halves
+        if(l >= r) return;                                                              // Base Case - only one element in the array
+        int mid = l + (r-l) / 2;                                                        // Dividing the array based on the middle index
+        divide(arr, aux, l, mid);                                                       // Dividing the array into left and right parts -
+        divide(arr, aux, mid+1, r);                                                     // - and merging the resulting sorted array in the -
+        conquer(arr, aux, l, mid, r);                                                   // - conquer function call, implemented above.
     }
-    void mergeSort(int[] arr) {
-        int[] aux = new int[arr.length];
-        divide(arr, aux, 0, arr.length-1);
+    void mergeSort(int[] arr) {                                                         // Wrapper function for the MergeSort algorithm
+        int[] aux = new int[arr.length];                                                // Creating the auxiliary array
+        divide(arr, aux, 0, arr.length-1);                                            // Initial bounds encompassing all the values in the array
     }
     public static void main(String[] args) {
-        MergeSort obj = new MergeSort();
-        int[] arr = new int[]{199, 28, 191, 20, 100};
-        obj.mergeSort(arr);
-        System.out.println(Arrays.toString(arr));
+        MergeSort obj = new MergeSort();                                                // Class object creation, alternative being static methods
+        int[] arr = new int[]{199, 28, 191, 20, 100};                                   // Initializing the array to be sorted
+        obj.mergeSort(arr);                                                             // MergeSort function call
+        System.out.println(Arrays.toString(arr));                                       // Printing the sorted array
     }
 }
--- Algorithms/Sorting/QuickSort.java
+++ Algorithms/Sorting/QuickSort.java
@@ -2,12 +2,9 @@ import java.util.Arrays;
 class QuickSort {
     // QuickSort - Divide and Conquer Algorithm
 
-    // It works by randomly picking any pivot element from the given array -
-    // - and then placing the pivot into its sorted position, and placing -
-    // - the values smaller than it to its left, and the values greater than -
-    // - it to its right. We then recursively call this partition and sort -
-    // - technique to all the elements in the range to the left of the pivot -
-    // - and to the elements in the range right of the pivot.
+    // It works by randomly picking any pivot element from the given array and then placing the pivot into its sorted position, and placing -
+    // - the values smaller than it to its left, and the values greater than it to its right. We then recursively call this partition and sort -
+    // - technique to all the elements in the range to the left of the pivot and to the elements in the range right of the pivot.
 
     // Time Complexity - Average Case - O(n log n), Worst Case - O(n ^ 2)
     // Space Complexity - Average Case - O(log n), Worst Case - O(n) (This space is for the recursion call stack, otherwise it is constant or in-place)
@@ -18,7 +15,7 @@ class QuickSort {
         arr[j] = temp;
     }
     void divide(int[] arr, int left, int right) {                       // Divides the array based on the pivot
-        if(left >= right) return;
+        if(left >= right) return;                                       // Base Case - only one element in the array or left bounds exceed the right bounds
         int pivot = (int) (Math.random()*(right-left+1)) + left;        // Calculating the pivot index randomly
         int pivotVal = arr[pivot];
 
@@ -39,7 +36,7 @@ class QuickSort {
     }
     public static void main(String[] args) {
         QuickSort obj = new QuickSort();                                // Creating the class object, alternatively, we can make the other functions static
-        int[] arr = new int[]{101, 29, 33, 107, 95, 96};
+        int[] arr = new int[]{101, 29, 33, 107, 95, 96};                // Initializing the array to be sorted array
         obj.quickSort(arr);                                             // QuickSort function call
         System.out.println(Arrays.toString(arr));                       // Printing the array using the toString method in the Arrays class
     }
